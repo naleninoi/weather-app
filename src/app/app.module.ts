5888565ba2969a7301ae546fafd1d428b1be7783
Viktor Kochetov-Arhipov <naleninoi@gmail.com>
2022-10-09 20:23:28 +0300

    service worker update p118

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,9 @@ import { environment } from '../environments/environment';
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
